42448d2d35e8cdd7ad318afb31211be635e7d615
thinkmassive <alex@thinkmassive.org>
2021-05-16 00:31:50 -0400

    /metrics: populate using prometheus_flask_exporter

--- api.py
+++ api.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, request
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
-app.config["DEBUG"] = True
+#app.config["DEBUG"] = True
+
+metrics = PrometheusMetrics(app)
+metrics.info('api_info', 'API info', version='0.1-alpha')
 
 @app.route('/')
 def home():
@@ -11,8 +15,11 @@ def home():
 def health():
     return "OK\n"
 
-@app.route('/metrics')
-def metrics():
-    return "metrics coming soon\n"
+metrics.register_default(
+    metrics.counter(
+        'by_path_counter', 'Request count by request paths',
+        labels={'path': lambda: request.path}
+    )
+)
 
 app.run()
