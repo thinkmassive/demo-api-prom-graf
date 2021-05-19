2829cd6863841682471cad36e89a8eba5cee8350
thinkmassive <alex@thinkmassive.org>
2021-05-19 14:41:24 -0400

    api: new endpoints (price, slow, fast)

--- api/api.py
+++ api/api.py
@@ -1,6 +1,11 @@
 from flask import Flask, request
 from prometheus_flask_exporter import PrometheusMetrics
 
+import json
+import random
+import time
+import urllib.request
+
 app = Flask(__name__)
 #app.config["DEBUG"] = True
 
@@ -13,6 +18,27 @@ def home():
 
 @app.route('/health')
 def health():
+    return "OK\n"
+
+@app.route('/price')
+def price():
+    response = ''
+    with urllib.request.urlopen("https://api.coindesk.com/v1/bpi/currentprice.json") as url:
+        coindesk_json = url.read().decode()
+        data = json.loads(coindesk_json)
+        response = data['bpi']['USD']['rate']
+    return response
+
+@app.route('/fast')
+def fast():
+    delay = random.random() * 2
+    time.sleep(delay)
+    return "OK\n"
+
+@app.route('/slow')
+def slow():
+    delay = random.randrange(5)
+    time.sleep(delay)
     return "OK\n"
 
 metrics.register_default(
